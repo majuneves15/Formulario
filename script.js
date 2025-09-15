50475b53ee6ebc9f9aa87041244617ec30b1ed70
majuneves15 <maria.neves.andrade@escola.pr.gov.br>
2025-09-15 12:00:04 -0300

    .

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ function processaTexto(texto) {
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
     return ordenadas.slice(0, 10);
 }
+
+function tiraPalavrasRuins(palavras) {
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
+}
+function contaFrequencias(palavras) {
+    const frequencias = {};
+
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    return frequencias;
+}
